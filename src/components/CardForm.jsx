6a0252b783e77dbca47aaa9cdc363084e952c2fe
Juan D. Garces Leon <jdgarces@marval.com.co>
2023-11-02 16:03:52 -0500

    new cardComponent

--- src/components/CardForm.jsx
+++ src/components/CardForm.jsx
@@ -1,122 +1,24 @@
-import * as React from 'react';
-import AspectRatio from '@mui/joy/AspectRatio';
-import Box from '@mui/joy/Box';
-import Button from '@mui/joy/Button';
-import Card from '@mui/joy/Card';
-import CardContent from '@mui/joy/CardContent';
-import Typography from '@mui/joy/Typography';
-import Sheet from '@mui/joy/Sheet';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/cardForm.css';
 
-export default function UserCard() {
+const CardForm = ({ fields }) => {
+
   return (
-    <Box
-      sx={{
-        width: '80%',
-        position: 'relative',
-        overflow: "hidden",
-      }}
-    >
-      <Box
-        sx={{
-          position: 'absolute',
-          display: 'block',
-          width: '1px',
-          bgcolor: 'warning.300',
-          left: '500px',
-          top: '-24px',
-          bottom: '-24px',
-          '&::before': {
-            top: '4px',
-            content: '"vertical"',
-            display: 'block',
-            position: 'absolute',
-            right: '0.5rem',
-            color: 'text.tertiary',
-            fontSize: 'sm',
-            fontWeight: 'lg',
-          },
-          '&::after': {
-            top: '4px',
-            content: '"horizontal"',
-            display: 'block',
-            position: 'absolute',
-            left: '0.5rem',
-            color: 'text.tertiary',
-            fontSize: 'sm',
-            fontWeight: 'lg',
-          },
-        }}
-      />
-      <Card
-        orientation="horizontal"
-        sx={{
-          width: '100%',
-          flexWrap: 'wrap',
-          [`& > *`]: {
-            '--stack-point': '500px',
-            minWidth:
-              'clamp(0px, (calc(var(--stack-point) - 2 * var(--Card-padding) - 2 * var(--variant-borderWidth, 0px)) + 1px - 100%) * 999, 100%)',
-          },
-          // make the card resizable for demo
-          overflow: 'auto',
-          resize: 'horizontal',
-        }}
-      >
-        <AspectRatio flex ratio="1" maxHeight={182} sx={{ minWidth: 182 }}>
-          <img
-            src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?auto=format&fit=crop&w=286"
-            srcSet="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?auto=format&fit=crop&w=286&dpr=2 2x"
-            loading="lazy"
-            alt=""
-          />
-        </AspectRatio>
-        <CardContent>
-          <Typography fontSize="xl" fontWeight="lg">
-            Alex Morrison
-          </Typography>
-          <Typography level="body-sm" fontWeight="lg" textColor="text.tertiary">
-            Senior Journalist
-          </Typography>
-          <Sheet
-            sx={{
-              bgcolor: 'background.level1',
-              borderRadius: 'sm',
-              p: 1.5,
-              my: 1.5,
-              display: 'flex',
-              gap: 2,
-              '& > div': { flex: 1 },
-            }}
-          >
-            <div>
-              <Typography level="body-xs" fontWeight="lg">
-                Articles
-              </Typography>
-              <Typography fontWeight="lg">34</Typography>
-            </div>
-            <div>
-              <Typography level="body-xs" fontWeight="lg">
-                Followers
-              </Typography>
-              <Typography fontWeight="lg">980</Typography>
-            </div>
-            <div>
-              <Typography level="body-xs" fontWeight="lg">
-                Rating
-              </Typography>
-              <Typography fontWeight="lg">8.9</Typography>
-            </div>
-          </Sheet>
-          <Box sx={{ display: 'flex', gap: 1.5, '& > button': { flex: 1 } }}>
-            <Button variant="outlined" color="neutral">
-              Chat
-            </Button>
-            <Button variant="solid" color="primary">
-              Follow
-            </Button>
-          </Box>
-        </CardContent>
-      </Card>
-    </Box>
+    <Link to="#" className="link-card">
+      <div className="card animate__animated animate__fadeInUp">
+        <div className="overflow">
+          <img src={""} alt="..." className="card-img-top" />
+        </div>
+        <div className="card-body">
+          <h4 className="card-title">{""}</h4>
+          <p className="card-text">{"description"}</p>
+          <p className="card-text">${"price"}</p>
+        </div>
+      </div>
+    </Link>
   );
-}
+};
+
+
+export default CardForm;
